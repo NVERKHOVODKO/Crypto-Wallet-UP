94120e4c1ed9ffdd26223c8642d5f953366771de
nikita_ve <nikita_ve@holdingbp.ru>
2024-04-07 18:43:59 +0300

    еще документация

--- UP/UP/Controllers/EmailController.cs
+++ UP/UP/Controllers/EmailController.cs
@@ -17,30 +17,53 @@ public class EmailController : ControllerBase
         _emailService = emailService;
     }
     
+    /// <summary>
+    /// Отправляет код верификации на указанный email.
+    /// </summary>
+    /// <param name="request">Запрос на отправку кода верификации.</param>
+    /// <returns>Результат операции отправки кода верификации.</returns>
     [HttpPost("sendVerificationCode")]
     [AllowAnonymous]
+
     public async Task<IActionResult> SendVerificationCode(SendVerificationCodeRequest request)
     {
         await _emailService.SendVerificationCode(request.Id);
         return Ok();
     }
 
+    /// <summary>
+    /// Подтверждает адрес электронной почты пользователя по коду верификации.
+    /// </summary>
+    /// <param name="request">Запрос на подтверждение адреса электронной почты.</param>
+    /// <returns>Результат операции подтверждения адреса электронной почты.</returns>
     [HttpPost("verifyEmail")]
     [AllowAnonymous]
+
     public async Task<IActionResult> VerifyEmail([FromBody] VerifyEmailRequest request)
     {
         await _emailService.VerifyEmail(request);
         return Ok();
     }
     
+    /// <summary>
+    /// Подтверждает восстановление пароля пользователя по коду верификации.
+    /// </summary>
+    /// <param name="request">Запрос на подтверждение восстановления пароля.</param>
+    /// <returns>Результат операции подтверждения восстановления пароля.</returns>
     [HttpPost("confirm-restore-password")]
     [AllowAnonymous]
+
     public async Task<IActionResult> ConfirmRestorePassword([FromBody] VerifyEmailRequest request)
     {
         await _emailService.VerifyEmail(request);
         return Ok();
     }
     
+    /// <summary>
+    /// Восстанавливает пароль пользователя по запросу на восстановление пароля.
+    /// </summary>
+    /// <param name="request">Запрос на восстановление пароля.</param>
+    /// <returns>Результат операции восстановления пароля пользователя.</returns>
     [HttpPatch("restore-password")]
     [AllowAnonymous]
     public async Task<IActionResult> RestorePassword([FromBody] RestorePasswordRequest request)
@@ -49,6 +72,11 @@ public class EmailController : ControllerBase
         return Ok();
     }
     
+    /// <summary>
+    /// Отправляет сообщение о блокировке пользователю.
+    /// </summary>
+    /// <param name="request">Запрос на отправку сообщения о блокировке.</param>
+    /// <returns>Результат операции отправки сообщения о блокировке.</returns>
     [HttpPatch("send-message-block")]
     [AllowAnonymous]
     public async Task<IActionResult> SendMessage([FromBody] SendMessageRequest request)

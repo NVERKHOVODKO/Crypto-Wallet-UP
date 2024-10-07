ad7835404fa6fa1f0ab11504e4c1ce93c35a2286
nikita_ve <nikita_ve@holdingbp.ru>
2024-10-07 15:18:25 +0300

    refactor: master - refactor

--- UP/UP/Exceptions/EntityNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectX.Exceptions;
-
-public class EntityNotFoundException : Exception
-{
-    public EntityNotFoundException(string message) : base(message)
-    {
-    }
-}
--- UP/UP/Exceptions/IncorrectDataException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectX.Exceptions;
-
-public class IncorrectDataException : Exception
-{
-    public IncorrectDataException(string message) : base(message)
-    {
-    }
-}
